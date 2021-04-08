8e50d0b4c16d0c015f2ebaeab83065d813fd70a1
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-08 18:52:11 +0200

    add keyboard number

--- main.js
+++ main.js
@@ -12,12 +12,34 @@ $(document).ready(function(){
         navSlide('next')
     });
 
-    //Navigation with Keyboard
+    //Navigation with Keyboard Arrows
     $(document).keydown(function(event){
         if(event.keyCode == 37){
             navSlide('prev');
         } else if(event.keyCode == 39){
             navSlide('next');
+        }
+    });
+
+    //Navigation with Keyboard Number
+    $(document).keydown(function(event){     
+        switch(event.keyCode){
+            case 97:
+            case 49:
+                keyboardNum(0);
+                break;
+            case 98:
+            case 50:
+                keyboardNum(1);
+                break;
+            case 99:
+            case 51:
+                keyboardNum(2);
+                break;
+            case 100:
+            case 52:
+                keyboardNum(3);
+                break;
         }
     });
 
@@ -81,4 +103,22 @@ function clickCircle(){
 
     $(this).addClass('active');
     image.eq($(this).index()).addClass('active');
+}
+
+/**
+ * Navigation with keyboard number
+ * @param {number} index 
+ */
+function keyboardNum(index){
+    var activeImg = $('.images .active');
+    var activeCircle = $('.nav .active');
+    var image = $('.images img');
+    var circle = $('.nav i')
+
+    //Reset
+    activeImg.removeClass('active');
+    activeCircle.removeClass('active');
+
+    image.eq(index).addClass('active');
+    circle.eq(index).addClass('active');
 }
