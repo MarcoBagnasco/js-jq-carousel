98928c549231b0b8f90eb1ba1628cfa59e64c37b
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-08 16:06:45 +0200

    add references

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    console.log($);//test
-})
+    
+    //REFERENCES
+    var prevBtn = $('.prev');
+    var nextBtn = $('.next');
+    console.log(prevBtn); //test 
+    console.log(nextBtn); //test
+
+
+//End Doc Ready
+});
