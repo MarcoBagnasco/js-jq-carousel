9d79bc3636d0d8210000e87619b9af06595238d0
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-08 16:08:50 +0200

    add click

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
-    
+
     //REFERENCES
     var prevBtn = $('.prev');
     var nextBtn = $('.next');
-    console.log(prevBtn); //test 
-    console.log(nextBtn); //test
+    // console.log(prevBtn); //test 
+    // console.log(nextBtn); //test
 
+    //Navigation
+    prevBtn.click(function(){
+        console.log('prev');
+    });
+    nextBtn.click(function(){
+        console.log('next');
+    });
 
 //End Doc Ready
 });
